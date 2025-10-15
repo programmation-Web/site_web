c5804e22fd3b33410437d538f49c29d894ec6449
wilsdefots <fotsing.wilson88@gmail.com>
2025-10-14 22:56:18 -0400

    deuxieme_commit

--- site_web/script-site.js
+++ site_web/script-site.js
@@ -1,14 +1,13 @@
 
-  const btn_a_propos = document.getElementById("aPropos");
-  const sousmenu = document.querySelector(".sousmenu"); 
-  
-  btn_a_propos.addEventListener("click", function(e) {
-      e.stopPropagation(); 
-      sousmenu.classList.toggle("sousmenu-open"); 
-  });
-  
-  document.addEventListener("click", function() {
-      sousmenu.style.display = "none";
-      sousmenu.classList.remove("sousmenu-open");
-  });
-  
+document.addEventListener("DOMContentLoaded", () => {
+    const btn_a_propos = document.getElementById("aPropos");
+    const sousmenu = document.querySelector(".sousmenu");
+
+    btn_a_propos.addEventListener("click", (e) => {
+        e.stopPropagation(); 
+        sousmenu.classList.toggle("sousmenu-open");
+    });
+    document.addEventListener("click", () => {
+        sousmenu.classList.remove("sousmenu-open");
+    });
+});
